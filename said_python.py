32619c2917aa2315d4f50fb2237199ba524285ec
Said-ichigo <ichigokurosake95@gmail.com>
2023-10-02 18:16:37 +0500

    taza 2 fail

---
+++ said_python.py
@@ -0,0 +1,11 @@
+import random
+
+A = random.randint(1,10)
+B = random.randint(1,10)
+
+if A == B:
+    print('ten boldi <:)>',
+          A,B)   
+else:
+    print('ten bolmadi!!!',
+          A,B)
